e0a5477fe9e6824aed99a6fcc4038c96e3506ef9
MuhRaihan001 <muhraihannurtaufiq@gmail.com>
2025-12-16 15:46:17 +0800

    Auto load API

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ const {
     loadEvents 
 } = require('./handler/client');
 const { loadCommands } = require('./handler/command');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { helmet } = require('helmet');
+const { loadApi } = require('./handler/api');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(helmet())
+app.use(express.json());
+
+loadApi(app);
 
 loadEvents();
 loadCommands();
---
+++ handler/api.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+const path = require("path");
+const dotenv = require("dotenv");;
+
+dotenv.config();
+
+function getFilesRecursively(dir) {
+    let results = [];
+    const files = fs.readdirSync(dir);
+
+    for (const file of files) {
+        const filePath = path.join(dir, file);
+        const stat = fs.statSync(filePath);
+
+        if (stat.isDirectory()) {
+            results = results.concat(getFilesRecursively(filePath));
+        } else if (file.endsWith(".js")) {
+            results.push(filePath);
+        }
+    }
+
+    return results;
+}
+
+/**
+ * Dynamically loads API routes into Express app
+ * @param {import("express").Express} app
+ */
+async function loadApi(app) {
+
+    const routeDir = path.join(__dirname, "../routes");
+    if (!fs.existsSync(routeDir))
+        return console.warn(`API directory not found: ${routeDir}`);
+
+    const routeFiles = getFilesRecursively(routeDir);
+    if (routeFiles.length === 0)
+        return console.warn(`No API files found in directory: ${routeDir}`);
+
+    console.log("Initiate API route loading...");
+
+    for (const routePath of routeFiles) {
+        const relativePath = path
+            .relative(routeDir, routePath)
+            .replace(/\\/g, "/");
+
+        const routeBasePath = `/api/${relativePath.replace(/\.js$/, "")}`;
+
+        try {
+            const routeModule = require(routePath);
+            app.use(routeBasePath, routeModule.default || routeModule);
+
+            console.log(`✅ | API : ${routeBasePath} Loaded`);
+        } catch (error) {
+            console.error(`Failed to load route file: ${routePath}`, error);
+        }
+    }
+}
+
+module.exports = { loadApi };
