183f849e3eba93198ac26a3cf4edac7e8337b902
ahmadanugrahsatya <ahmadanugrahsatya@gmail.com>
2025-12-17 19:01:28 +0800

    angga sudah pusing dengan error nya

--- handler/server.js
+++ handler/server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
--- routes/register.js
+++ routes/register.js
@@ -1,8 +1,45 @@
 var express = require('express');
+var bcrypt = require('bcrypt');
 var router = express.Router();
+const sanitizeHtml = require('sanitize-html');
 
-router.get('/', function(req,res,next){
-    res.render('register', {title:'Register'})
+router.get('/', function (req, res, next) {
+    res.render('register', { title: 'Register' })
 })
 
+router.post('/', async function (req, res, next) {
+    const db = req.app.locals.db;
+
+    try {
+        const username = req.body.username;
+        const password = req.body.acc;
+        const email = req.body.email;
+        const nama_depan = req.body.namaDepan;
+        const nama_belakang = req.body.namaBelakang;
+        const role = 'user';
+
+        const saltRounds = 10;
+
+        const password_hash = await bcrypt.hash(password, saltRounds);
+
+        const query = "INSERT INTO user (username, nama_depan, nama_belakang, password, email, role) VALUES (?, ?, ?, ?, ?, ?)";
+        const params = [username, nama_depan,nama_belakang, password_hash, email, role];
+
+        db.query(query, params, (err, result) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ message: "Gagal menyimpan" });
+            } else {
+
+            }
+
+            res.redirect('/login');
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error server" });
+    }
+
+})
 module.exports = router;
